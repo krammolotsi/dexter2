41818ab149c37c4f580d0987cf522d84b0bc776e
krammolotsi <krammolotsi@gmail.com>
2023-04-12 10:57:11 +0200

    undo

--- pages/api/copynpaypost.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const cors  = require('cors');
-
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
