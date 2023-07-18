1feb44278d4c0f30556450bc4a4a769925b7889a
krammolotsi <krammolotsi@gmail.com>
2023-07-18 14:51:40 +0200

    apple pay2

--- pages/copynpay/applepay.js
+++ pages/copynpay/applepay.js
@@ -1,5 +1,5 @@
 import Script from "next/script";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from 'react'
 
 function applepay() {
@@ -9,10 +9,7 @@ function applepay() {
     let lnk = "";
     let ndc = "";
     let redirect = "";
-    let wpw = ` var wpwlOptions = {${applePay}:{
-        displayName: "MyStore",
-        total: { label: "COMPANY, INC." }}
-    }`
+   
   //set link for script
   lnk = `https://eu-prod.oppwa.com/v1/paymentWidgets.js?checkoutId=1D0B019DAF32BBD1DD4950DB3EEC280B.prod01-vm-tx18`;
 
@@ -27,29 +24,8 @@ function applepay() {
         className="paymentWidgets"
         data-brands="APPLEPAY"
       ></form>
-      <Script src={wpw}>
-      </Script>
     </div>
   );
-
-  /*
-  
-
-   <form action="https://www.peachpayments.com/" class="paymentWidgets" data-brands="APPLEPAY"></form>
-        <script src="https://eu-prod.oppwa.com/v1/paymentWidgets.js?checkoutId=FECF7EF6422012758550B7744D49E25A.prod01-vm-tx18"></script> 
-        
-        <script src="https://code.jquery.com/jquery.js" type="text/javascript"></script>
-<script type="text/javascript"> 
- var wpwlOptions = {
-  applePay: {
-    displayName: "MyStore",
-    total: { label: "COMPANY, INC." }
-  }
-}
-       
-</script> 
-  
-  */
 
   return (
     <div>{widget}</div>
