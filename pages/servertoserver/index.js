d7cea5933759afb6a07905edfe992de69fa43717
krammolotsi <krammolotsi@gmail.com>
2024-02-28 11:18:15 +0200

    google pay checkout

--- pages/servertoserver/index.js
+++ pages/servertoserver/index.js
@@ -1,7 +1,24 @@
 import Dash from "../dash"
+import Script from "next/script";
 
 export default function servertoserver() {
   return (
-    <Dash children={ <h1>Pending integration ServerToServer</h1>} />
+    <Dash children={ <div>
+<form action="https://www.peachpayments.com" class="paymentWidgets" data-brands="VISA MASTER GOOGLEPAY"></form>
+        
+        <Script src="https://eu-prod.oppwa.com/v1/paymentWidgets.js?checkoutId=7B3AE08CF60DCDCDB61390ECCE875640.prod01-vm-tx13"></Script>     
+          <Script>
+           onLoad={() => {
+         wpwlOptions = {
+           style:"card",
+           disableCardExpiryDateValidation: true,
+         googlePay: {
+               gatewayMerchantId: "8acda4c78262a03d01828ce3db993865",
+               merchantId: "BCR2DN4TXWQZX5BQ"
+           }
+       }
+         }}
+           </Script> 
+    </div>} />
   )
 }
