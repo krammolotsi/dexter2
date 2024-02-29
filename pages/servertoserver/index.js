71be0d4d514dd5945e404a71e1572e5d36f6c88c
krammolotsi <krammolotsi@gmail.com>
2024-02-29 13:42:35 +0200

    applePay

--- pages/servertoserver/index.js
+++ pages/servertoserver/index.js
@@ -4,10 +4,10 @@ import Script from "next/script";
 export default function servertoserver() {
   return (
     <Dash children={ <div>
-<form action="https://www.peachpayments.com" class="paymentWidgets" data-brands="VISA MASTER GOOGLEPAY"></form>
+<form action="https://www.peachpayments.com" class="paymentWidgets" data-brands="APPLEPAY"></form>
         
-        <Script src="https://eu-test.oppwa.com/v1/paymentWidgets.js?checkoutId=41BF5D73230C4495B43A0089FD8FAE03.uat01-vm-tx03"></Script>     
-          <Script>
+        <Script src="https://eu-prod.oppwa.com/v1/paymentWidgets.js?checkoutId=1C0E44AE887ED736818BBB1EA0EEC78D.prod01-vm-tx14"></Script>     
+          {/* <Script>
            onLoad={() => {
          wpwlOptions = {
            style:"card",
@@ -18,7 +18,7 @@ export default function servertoserver() {
            }
        }
          }}
-           </Script> 
+           </Script>  */}
     </div>} />
   )
 }
